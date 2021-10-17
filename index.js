e8710951776a6d77345855a52ec15bca7ecbe71c
Tuan Anh <18520446@gm.uit.edu.vn>
2021-10-17 16:07:52 +0700

    tuan anh last update

--- index.js
+++ index.js
@@ -27,21 +27,21 @@ const getData = async()=>{
   const pair1 = await contractPair1.methods.getReserves().call();
   const pair2 = await contractPair2.methods.getReserves().call();
   const pair3 = await contractPair3.methods.getReserves().call();
-  // k1 = pair1[0]/1000000000000000000//WETH
-  // k2 = pair1[1]/1000000//USDT
-  // k3 = pair2[1]/1000000//USDT
-  // k4 = pair2[0]/100000000//WBTC
-  // k5 = pair3[0]/100000000//WBTC
-  // k6 = pair3[1]/1000000000000000000//WETH
-  // console.log(`pair1: WETH: ${k1} - USDT ${k2}`)
-  // console.log(`pair2: USDT: ${k3} - WBTC ${k4}`)
-  // console.log(`pair3: WBTC: ${k5} - WETH ${k6}`)
-  k1 = 50000;
-  k2 = 100000000;
-  k3 = 85000;
-  k4 = 2.18124;
-  k5 = 1885.824;
-  k6 = 40883;
+  k1 = pair1[0]/1000000000000000000//WETH
+  k2 = pair1[1]/1000000//USDT
+  k3 = pair2[1]/1000000//USDT
+  k4 = pair2[0]/100000000//WBTC
+  k5 = pair3[0]/100000000//WBTC
+  k6 = pair3[1]/1000000000000000000//WETH
+  console.log(`pair1: WETH: ${k1} - USDT ${k2}`)
+  console.log(`pair2: USDT: ${k3} - WBTC ${k4}`)
+  console.log(`pair3: WBTC: ${k5} - WETH ${k6}`)
+  // k1 = 50000;
+  // k2 = 100000000;
+  // k3 = 85000;
+  // k4 = 2.18124;
+  // k5 = 1885.824;
+  // k6 = 40883;
   calculate_x(k1,k2,k3,k4,k5,k6)
 }
 getData()
